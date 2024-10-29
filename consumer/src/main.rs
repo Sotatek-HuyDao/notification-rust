87137d308a74fbcad5058d67e92ac7dafad3a8d6
Sotatek-HuyDao <huy.dao@sotatek.com>
2024-10-29 17:54:45 +0700

    add: consumer

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod model;
+mod routes;
+use crate::model::QueryRoot;
+use crate::routes::{graphql_handler, graphql_playground, health};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use axum::{extract::Extension, routing::get, Router, Server};
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    let app = Router::new()
+        .route("/", get(graphql_playground).post(graphql_handler))
+        .route("/health", get(health))
+        .layer(Extension(schema));
+
+    Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
